524a9874750f03486af6ea03ad1ad18c42b82582
Bottersnike <>
2017-07-02 22:23:02 +0100

    Extent load_names

--- main.py
+++ main.py
@@ -2,4 +2,8 @@
 def load_names():
 	with open("namepool.txt") as namepool_file:
 	    names = namepool_file.read().split("\n")
-	    return names
+	
+    while "" in names:  # In case of newline at end of file
+        names.remove("")
+
+    return names
